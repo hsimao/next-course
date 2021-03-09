35763c749b135ab31eef5e17f6123b2bc24ed0a9
hsimao <e087754958@gmail.com>
2021-03-09 21:00:27 +0800

    add: events search

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,17 +1,28 @@
 import Head from "next/head";
+import { Fragment } from "react";
+import { useRouter } from "next/router";
 import { getFeaturedEvents } from "../../dummy-data";
 import EventList from "../../components/Events/EventList";
+import EventsSearch from "../../components/Events/EventsSearch";
 
 export default function AllEventsPage() {
-  const featuredEvents = getFeaturedEvents();
+  const router = useRouter();
+  const events = getFeaturedEvents();
+
+  const findEvents = ({ year, month }) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
 
   return (
-    <div>
+    <Fragment>
       <Head>
         <title>All Events Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <EventList items={featuredEvents} />
-    </div>
+      <EventsSearch onSearch={findEvents} />
+      <EventList items={events} />
+    </Fragment>
   );
 }
