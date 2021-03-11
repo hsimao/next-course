3546d8f05e4e601e906a7280fb786aa538a18557
hsimao <e087754958@gmail.com>
2021-03-11 21:24:22 +0800

    add: ISR 模式與 client side 的 fetch 方式

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,20 +1,35 @@
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-export default function LastSalesPage() {
-  const [sales, setSales] = useState(false);
+const transformSalesData = (sales) => {
+  return Object.keys(sales).map((key) => ({
+    id: key,
+    ...sales[key]
+  }));
+};
 
-  const transformSalesData = (sales) => {
-    return Object.keys(sales).map((key) => ({
-      id: key,
-      ...sales[key]
-    }));
+// 第一次在 build time fetch data
+export async function getStaticProps() {
+  const sales = await fetch(
+    "https://react-course-ddef7-default-rtdb.firebaseio.com/sales.json"
+  )
+    .then((res) => res.json())
+    .then((data) => transformSalesData(data));
+
+  return {
+    props: { sales },
+    revalidate: 10 // 後續將在每次 user 請求該頁面時重新 fetch data (10 秒內最多執行一次)
   };
+}
+
+export default function LastSalesPage(props) {
+  const [sales, setSales] = useState(props.sales);
 
   const { data, error } = useSWR(
     "https://react-course-ddef7-default-rtdb.firebaseio.com/sales.json"
   );
 
+  // client side fetch data
   useEffect(() => {
     if (data) {
       setSales(transformSalesData(data));
@@ -25,7 +40,7 @@ export default function LastSalesPage() {
     return <p>Failed to load.</p>;
   }
 
-  if (!data || !sales) {
+  if (!data && !sales) {
     return <p>Loading...</p>;
   }
 
